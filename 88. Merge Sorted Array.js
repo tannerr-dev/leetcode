ff1ef80d3f603e14fea103e171670464babdb1a1
Tanner Reyhons <tannerr.dev@gmail.com>
2024-02-16 23:40:15 -0600

    this passed submission but is not ranked high lol

--- 88. Merge Sorted Array.js
+++ 88. Merge Sorted Array.js
@@ -4,6 +4,7 @@ let arr2 = [1,2,2];
 let b = 3;
 
 var merge = function(nums1, m, nums2, n) {
+    nums1.splice(m, n)
     let count = n-1;
     let j = 0;
     while (count >=0){
@@ -15,14 +16,6 @@ var merge = function(nums1, m, nums2, n) {
             j=0
         }
     }
-    count= m + n + n - 1;
-    while(count>=0){
-        if(nums1[count]==0){
-            nums1.splice(count, 1)
-        } else {
-            count--;
-        }
-    }
-    console.log(nums1)
-}
+
+};
 merge(arr1, a, arr2, b);
