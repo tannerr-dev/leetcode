aeb3e978929cfbe51c1c8a91c11618892c75496d
Tanner Reyhons <tannerr.dev@gmail.com>
2024-02-16 23:22:50 -0600

    second test yields correct results IMO

--- 88. Merge Sorted Array.js
+++ 88. Merge Sorted Array.js
@@ -1,6 +1,6 @@
-let arr1 = [1, 2, 3, 0, 0, 0];
-let arr2 = [2, 5, 6];
-let a = 3;
+let arr1 = [-1,0,0,3,3,3,0,0,0];
+let a = 6;
+let arr2 = [1,2,2];
 let b = 3;
 
 var merge = function(nums1, m, nums2, n) {
@@ -16,7 +16,6 @@ var merge = function(nums1, m, nums2, n) {
         }
     }
     count= m + n + n - 1;
-    // console.log(count)
     while(count>=0){
         if(nums1[count]==0){
             nums1.splice(count, 1)
@@ -24,33 +23,6 @@ var merge = function(nums1, m, nums2, n) {
             count--;
         }
     }
-    // console.log(nums2)
     console.log(nums1)
 }
-let output = merge(arr1, a, arr2, b);
-
-
-
-
-
-
-
-
-
-
-// var notmerge = function(nums1, m, nums2, n) { for(let j = m + n; j >= 0; j--){
-//         for(let i = n - 1; i >= 0; i-- ){
-//             // console.log(nums2[i])
-//             if(nums2[i]>nums1[j]){
-//             }
-               
-//             console.log(nums2.shift())
-//         }
-//     }
-//     nums1.splice(0, 0, 'butts')
-//     console.log(nums1)
-//     let result = 'butts'
-//     // console.log(typeof result);
-//     // console.log(result)
-//     return result;
-// }
+merge(arr1, a, arr2, b);
